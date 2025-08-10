40d54c0d05123764eb08223615cb391ec3885666
mykaachan <163104957+mykaachan@users.noreply.github.com>
2025-08-10 16:27:27 +0800

    Add files via upload

--- products/product/urls.py
+++ products/product/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from .views import ProductTestView, AddProductView
+from .views import ProductTestView, AddProductView, DeleteProductView, UpdateProductView, ReadProductView
 
 urlpatterns = [
    path('product-test/',ProductTestView.as_view(), name='product-test'),  # For product-related URLs
    path('add_product/', AddProductView.as_view(), name='add-product'),  # For adding a product
+   path('delete_product/', DeleteProductView.as_view(), name='delete-product'),  # For deleting a product
+   path('update_product/', UpdateProductView.as_view(), name='update-product'),  # For updating a product
+   path('read_product/', ReadProductView.as_view(), name='read-product'),  # For reading a product
 ]
 
 
--- products/product/views.py
+++ products/product/views.py
@@ -2,10 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProductSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
+from .serializers import ProductSerializer, UpdateProductSerializer
+from products.models import Product 
 
 class AddProductView(APIView):
-      def post(self, request):
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -24,6 +28,39 @@ class ProductTestView(APIView):
     def post(self, request):
         return Response({"message": "POST product works too!"})
     
+class DeleteProductView(APIView):
+    def delete(self, request, *args, **kwargs):
+        product_id = request.data.get('id')
+        if not product_id:
+            return Response({"error": "Product id is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            product = Product.objects.get(id=product_id)
+            product.delete()
+            return Response({"message": "Product deleted"}, status=status.HTTP_200_OK)
+        except Product.DoesNotExist:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
+class UpdateProductView(APIView):
+    def put(self, request, *args, **kwargs):
+        serializer = UpdateProductSerializer(data=request.data)
+        if serializer.is_valid():
+            product_id = request.data.get('id')
+            try:
+                product = Product.objects.get(id=product_id)
+                serializer.update(product, serializer.validated_data)
+                return Response({"message": "Product updated successfully"}, status=status.HTTP_200_OK)
+            except Product.DoesNotExist:
+                return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-   
+    
+class ReadProductView(APIView):
+    def get(self, request, *args, **kwargs):
+        product_id = request.data.get('id')
+        try:
+            product = Product.objects.get(id=product_id)
+            serializer = ProductSerializer(product)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Product.DoesNotExist:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
